80b6b28c1529255cc53d028eed6b7b69f187ac33
Jordan <jmena@leonext.net>
2026-08-30 22:06:42 -0400

    Crear estructura inicial del proyecto

---
+++ citas/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Cliente, Servicio
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'telefono')
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'duracion_min')
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
