122c8404d9b65270ac14155c9a3475ff42258fc6
Jordan <jmena@leonext.net>
2026-08-31 01:48:19 -0400

    Feat: Creacion del modelo Cita

--- citas/models.py
+++ citas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
@@ -14,3 +15,22 @@ class Servicio(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Cita(models.Model):
+
+    class Estado(models.TextChoices):
+        PENDIENTE = 'pendiente', 'Pendiente'
+        CONFIRMADA = 'confirmada', 'Confirmada'
+        ATENDIDA = 'atendida', 'Atendida'
+    
+    #Si un cliente es borrado no debe quedar registro en la aplicacion, por la ley 21.719.
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='citas')
+    #Mantiene un historial para tomar decisiones de negocio.
+    servicio = models.ForeignKey(Servicio, on_delete=models.PROTECT)
+    estado = models.CharField(max_length=10, choices=Estado.choices, default=Estado.PENDIENTE)
+    fecha = models.DateTimeField()
+    creacion = models.DateTimeField(auto_now_add=True)
+    ultima_modificacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.cliente.nombre} - {self.servicio.nombre} - {timezone.localtime(self.fecha).strftime('%d/%m/%Y %H:%M')}"
