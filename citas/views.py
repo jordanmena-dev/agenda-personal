97b4a871bfc7ec46c5ac3aef85c9e0b43b05c459
Jordan <jmena@leonext.net>
2026-08-31 03:15:01 -0400

    Feat: Creacion de vista listado de servicios con IVA

--- citas/views.py
+++ citas/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Servicio
 
-# Create your views here.
+def lista_servicios(request):
+    servicios = Servicio.objects.all()
+
+    texto = ""
+    for servicio in servicios:
+        precio_iva = round(float(servicio.precio) * 1.19)
+        texto += f"{servicio.nombre} - precio: {servicio.precio} - precio (IVA): {precio_iva}<br>"
+
+    return HttpResponse(texto)
