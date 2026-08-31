b1281b687a4f6d30f72f9b71e8c76aef570954e1
Jordan <jmena@leonext.net>
2026-08-31 00:42:28 -0400

    Feat: Ordena servicios por precio en admin

--- citas/admin.py
+++ citas/admin.py
@@ -6,8 +6,9 @@ class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'telefono')
     search_fields = ('nombre',)
     ordering = ('nombre',)
+
 @admin.register(Servicio)
 class ServicioAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio', 'duracion_min')
     search_fields = ('nombre',)
-    ordering = ('nombre',)
+    ordering = ('precio',)
